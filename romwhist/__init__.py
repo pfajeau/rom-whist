0da5f88e7406942cbcc365d978f25783a38c668f
pfajeau <pfajeau@gmail.com>
2020-12-21 18:36:37 -0800

    Changed header and imports

--- romwhist/__init__.py
+++ romwhist/__init__.py
@@ -24,14 +24,14 @@ socketio = SocketIO(app, logger=True)
 #socketio = SocketIO(app)
 
 #from .routes import *
-from ohell import ohell_routes
+from .ohell import ohell_routes
 
 app.add_url_rule('/', view_func=ohell_routes.ohell_start, methods=["GET", "POST"])
 app.add_url_rule('/ohell_start', view_func=ohell_routes.ohell_start, methods=["GET", "POST"])
 app.add_url_rule('/ohell_play', view_func=ohell_routes.ohell_play, methods=["GET", "POST"])
 
 # When adding those, events are not received by the client anymre
-from belote import belote_routes
+from .belote import belote_routes
 app.add_url_rule('/belote_start', view_func=belote_routes.belote_start, methods=["GET", "POST"])
 app.add_url_rule('/belote_play', view_func=belote_routes.belote_play, methods=["GET", "POST"])
 
