95a96849d04dada1991fe6ae478068322e878cd4
pfajeau <pfajeau@gmail.com>
2021-02-03 18:02:47 -0800

    REversed belote_ai to call parent for play and bet until implementation complete

--- romwhist/belote/belote_ai.py
+++ romwhist/belote/belote_ai.py
@@ -1,7 +1,9 @@
 import logging
+import json
 
 from romwhist.ai.ai_player import AiPlayer
-
+from romwhist.ai.ai_agents import SimpleAgent, SimpleMCTSAgent, random_action
+from romwhist.belote.belote_state import BeloteState
 
 class BeloteAiPlayer(AiPlayer):
 
@@ -10,26 +12,23 @@ class BeloteAiPlayer(AiPlayer):
         self.__agent = SimpleMCTSAgent('BeloteSim', name,
                                        action_chooser_function='random_action',
                                        num_simulations=100)
-        self.__agent2 = SimpleMCTSAgent('BeloteSim', name,
-                                       action_chooser_function='random_action',
-                                       num_simulations=100)
         self.game_state = BeloteState(game_id, self.name)
 
 
     def player_to_bet(self, allowed_bets, game_state_json):
-        logging.debug("Belote AI PLayer to bet: %s", self.name)
-        logging.debug("Game state: %s", game_state_json)
-        game_state = BeloteState(**json.loads(game_state_json))
-        self.game_state = game_state
-
-        # this is here because easier for testing.
-        # allowed_bets shoud really come from the game state
-        for bet in allowed_bets:
-            self.game_state.allowed_bets.append(str(bet))
-
-        bet = self.compute_bet_agent()
-        logging.info("Agent calculated bet: %s", bet)
-        return bet
+        # logging.debug("Belote AI PLayer to bet: %s", self.name)
+        # logging.debug("Game state: %s", game_state_json)
+        # game_state = BeloteState(**json.loads(game_state_json))
+        # self.game_state = game_state
+        #
+        # # this is here because easier for testing.
+        # # allowed_bets shoud really come from the game state
+        # for bet in allowed_bets:
+        #     self.game_state.allowed_bets.append(str(bet))
+        #
+        # bet = self.compute_bet_agent()
+        # logging.info("Agent calculated bet: %s", bet)
+        return super().player_to_play(allowed_bets, game_state_json)
 
     def compute_bet_agent(self):
         return self.__agent2.get_bet(self.game_state)
@@ -42,4 +41,5 @@ class BeloteAiPlayer(AiPlayer):
         game_state = BeloteState(**json.loads(game_state_json))
         self.game_state = game_state
         self.game_state.allowed_cards = allowed_cards
+        return super().player_to_play(allowed_cards, game_state_json)
         return self.__agent.get_action(self.game_state)
